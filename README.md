026e60fb7e9811b36fd8c8a78d0e53fc067985eb
Helen <dd040388kev@gmail.com>
2022-07-27 12:02:03 +0300

    ready

---
+++ README.md
@@ -0,0 +1,7 @@
+https://monosnap.com/file/TAtnKOt4wwGJ47CjQEqdQ8fObUIk5V
+
+https://monosnap.com/file/Ahe78PVT9pcgzDbnSmEW2PmZC938ud
+
+https://monosnap.com/file/MU0wp0UIif1ukRJ5b18VJQCraAKNbv
+
+https://monosnap.com/file/NnY5EMsFbJvG5Ay2CAUWUSMayo5UIJ
--- index.js
+++ index.js
@@ -1,30 +1,45 @@
-const { string } = require("yargs");
 const contactsOperations = require("./db/contacts");
+const { Command } = require("commander");
+const program = new Command();
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
 
-const argv = require("yargs").argv;
+program.parse(process.argv);
+
+const argv = program.opts();
 
 async function invokeAction({ action, id, name, email, phone }) {
   switch (action) {
     case "list":
       const contacts = await contactsOperations.listContacts();
       console.table(contacts);
-          break;
-      
-      case "get":
-          const contact = await contactsOperations.getContactById(id);
-          if (!contact) {
-              throw new Error(`Contact with id=${id} not found`);
-          }
-          console.log(contact);
-          break;
-      
-    // case "add":
-    //   // ... name email phone
-    //   break;
+      break;
 
-    // case "remove":
-    //   // ... id
-    //   break;
+    case "get":
+      const contact = await contactsOperations.getContactById(id);
+      if (!contact) {
+        throw new Error(`Contact with id=${id} not found`);
+      }
+      console.log(contact);
+      break;
+
+    case "add":
+      const newContact = await contactsOperations.addContact(
+        name,
+        email,
+        phone
+      );
+      console.log("newContact", newContact);
+      break;
+
+    case "remove":
+      const removeContact = await contactsOperations.removeContact(id);
+      console.log(removeContact);
+      break;
 
     default:
       console.warn("\x1B[31m Unknown action type!");
@@ -32,13 +47,4 @@ async function invokeAction({ action, id, name, email, phone }) {
 }
 
 invokeAction(argv);
-// invokeAction({action: "get", id: "4"});
 
-// node index.js --action list
-// node index.js --action get --id 5
-
-// # Добавялем контакт
-// node index.js --action add --name Mango --email mango@gmail.com --phone 322-22-22
-
-// # Удаляем контакт
-// node index.js --action remove --id=3
